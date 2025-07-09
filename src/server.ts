ef41a13813b061231a7901317e6dcdfa03d6dd7d
leokbral <cabralleonardoferreira@gmail.com>
2025-07-09 12:09:36 -0300

    feat: add rooms listing

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import fastify from "fastify";
+import { sql } from "./db/connection.ts";
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
+import fastifyCors from "@fastify/cors";
+import { env } from "./env.ts";
+import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+    origin: '*', // Allow all origins
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get('/health', () => {
+    return { status: 'ok' };
+});
+
+app.register(getRoomsRoute);
+
+app.listen({ port: env.PORT })
